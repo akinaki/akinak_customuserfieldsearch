4e8efe083a7cb9e1dbfe77dd32fd67ed8dcc5fb6
Akinak <akinak@donapex.com>
2013-04-25 15:20:07 +0300

    First commit

--- Fake.php
+++ Fake.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /* for IDE autocomplete */
 class XFCP_Akinak_CustomUserFieldSearch_ControllerPublic_Member extends XenForo_ControllerPublic_Member {}
 class XFCP_Akinak_CustomUserFieldSearch_Model_User extends XenForo_Model_User {}
